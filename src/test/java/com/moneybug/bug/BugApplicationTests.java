f328bab4128da9083a7688fe97c0911d7f56e2a5
험버트씨네 저장소 <lawyeruh@naver.com>
2024-05-22 20:11:26 +0900

    first commit

---
+++ src/test/java/com/moneybug/bug/BugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneybug.bug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
